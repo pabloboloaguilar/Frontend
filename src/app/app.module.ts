e06c19a795c681b7f0527bb8499536698ee7b9f0
pabloboloaguilar <usbelpablob@gmail.com>
2023-05-14 08:32:04 -0300

    se agregaron edu-exp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { TrabajoEstudioComponent } from './components/trabajo-estudio/trabajo-estudio.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 
 @NgModule({
@@ -19,10 +20,11 @@ import { HysComponent } from './components/hys/hys.component';
     BarraComponent,
     HeaderComponent,
     AcercaDeComponent,
-    TrabajoEstudioComponent,
     ProyectosComponent,
     FooterComponent,
-    HysComponent
+    HysComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/experiencia/experiencia.component.spec.ts
+++ src/app/components/experiencia/experiencia.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TrabajoEstudioComponent } from './trabajo-estudio.component';
+import { ExperienciaComponent } from './experiencia.component';
 
-describe('TrabajoEstudioComponent', () => {
-  let component: TrabajoEstudioComponent;
-  let fixture: ComponentFixture<TrabajoEstudioComponent>;
+describe('ExperienciaComponent', () => {
+  let component: ExperienciaComponent;
+  let fixture: ComponentFixture<ExperienciaComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TrabajoEstudioComponent ]
+      declarations: [ ExperienciaComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TrabajoEstudioComponent);
+    fixture = TestBed.createComponent(ExperienciaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
